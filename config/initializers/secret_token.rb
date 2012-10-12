95056a943c98d9381b98defa0d9477cf57d474e0
Michael de Silva <michael@mwdesilva.com>
2012-10-12 19:25:36 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peritias::Application.config.secret_token = '442ff047587c6fd9caa4d13947883b9ead2551d571fc04490fb7f89c80fe2ee5155394de442206c2cc6bc2eb0d7b06f237d68e980542ccfb1d2bcafdfdac8ea7'
