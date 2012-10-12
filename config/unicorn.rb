cf07ceb64b0b2ec35fc95e5dd83e1aa0f5f31fb2
Michael de Silva <michael@mwdesilva.com>
2012-10-12 21:47:47 +0000

    update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
-working_directory "/peritias"
-pid "/tmp/pids/unicorn.pid"
-stderr_path "/log/unicorn.log"
-stdout_path "/log/unicorn.log"
+working_directory "/home/appuser/sites/peritias"
+pid "/home/appuser/sites/peritias/tmp/pids/unicorn.pid"
+stderr_path "/home/appuser/sites/peritias/log/unicorn.log"
+stdout_path "/home/appuser/sites/peritias/log/unicorn.log"
 
-listen "/tmp/unicorn.peritias.sock"
+listen "/home/appuser/sites/peritias/tmp/unicorn.peritias.sock"
 worker_processes 2
 timeout 30
